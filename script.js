e0adb5536317c7a43503de0c98aed136dc647bd3
Muhammad Ishaq Khan <aonemobilesncomputers786@gmail.com>
2025-08-19 23:48:37 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+   if ('serviceWorker' in navigator) {
+      navigator.serviceWorker.register('./sw.js', { scope: './' })
+        .then(reg => console.log('SW registered:', reg.scope))
+        .catch(err => console.error('SW failed:', err));
+    }
+
+
 // Automatic Slideshow
 let myIndex = 0;
 carousel();

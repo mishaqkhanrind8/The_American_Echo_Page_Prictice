226177bd9fb6b9271f3fb7959b1a19543f711211
Muhammad Ishaq Khan <aonemobilesncomputers786@gmail.com>
2025-08-29 09:31:32 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-   if ('serviceWorker' in navigator) {
-      navigator.serviceWorker.register('./sw.js', { scope: './' })
-        .then(reg => console.log('SW registered:', reg.scope))
-        .catch(err => console.error('SW failed:', err));
-    }
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/sw.js', { scope: '/' })
+    .then(reg => console.log('✅ Service Worker registered with scope:', reg.scope))
+    .catch(err => console.error('❌ Service Worker registration failed:', err));
+}
 
 
 // Automatic Slideshow
